edb73d0f18a77b5ed89e689831b7df31b4729862
Rizki Nabil Aufa <57241186+rizkinabil@users.noreply.github.com>
2024-02-13 11:53:45 +0700

    Time: 62 ms (38.91%), Space: 51.8 MB (7.10%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.ts
@@ -0,0 +1,18 @@
+function isSubsequence(s: string, t: string): boolean {
+    
+    if(s.length > t.length){
+        return false
+    }
+    
+    let j=0    
+    
+    for(let i=0; i < t.length; i++){
+            if(s[j] === t[i]){
+                j++
+                
+            }
+        }
+    
+    
+    return j === s.length
+};
